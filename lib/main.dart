12bdaf1af1fcab58f6e860b288d8fe255eafcffc
juliusayang3 <juliusayang3@gmail.com>
2021-12-15 01:29:13 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:stock/pages/stocks.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: Stocks(),
+  ),);
+}
+
+
---
+++ lib/pages/stocks.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Stocks extends StatefulWidget {
+  const Stocks({ Key key }) : super(key: key);
+
+  @override
+  _StocksState createState() => _StocksState();
+}
+
+class _StocksState extends State<Stocks> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
